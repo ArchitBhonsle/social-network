efba0eda099aa5843f59dc8242b6a348be748f5b
Archit Bhonsle <abhonsle2000@gmail.com>
2020-06-20 10:32:47 +0530

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/navbar/navbar.component';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
